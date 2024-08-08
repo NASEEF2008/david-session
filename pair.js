5fe6b81ef86f4b57c4b2811888e6df67c2a71746
NASEEF2008 <156422467+NASEEF2008@users.noreply.github.com>
2024-08-08 04:35:06 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -70,7 +70,7 @@ router.get('/', async (req, res) => {
 Please Follow My Support Channel
 Wanna talk to me?👉 https://wa.link/i7dtpv 👈
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-2099 David Cyril*
+©*2024-2099 ANY FC*
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
